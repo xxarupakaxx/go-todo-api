1bada568d6867d0d19dfe346d0e6b20868f8a50a
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-07-05 10:06:11 +0900

    UpdatedAtの修正

--- src/model/task.go
+++ src/model/task.go
@@ -3,8 +3,8 @@ package model
 import "time"
 
 type Task struct {
-	ID int `json:"id"`
+	ID        uint       `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
-	updatedAt time.Time `json:"updated_at"`
-	Title string `json:"title"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Title     string    `json:"title"`
 }
