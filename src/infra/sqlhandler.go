0853ba3c0e4ed5989cbe1dc888e38c364518b697
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-10-18 01:42:10 +0900

    🍵sqlhandler.go

--- src/infra/sqlhandler.go
+++ src/infra/sqlhandler.go
@@ -1,1 +1,28 @@
 package infra
+
+import (
+	"database/sql"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+type SqlHandler struct {
+	Conn *sql.DB
+}
+
+func NewSqlHandler() *SqlHandler {
+	err:=godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dbDriver:="mysql"
+	dbOption:="?parseTime=true"
+	conn,err:=sql.Open(dbDriver,os.Getenv("DB_USER")+":"+os.Getenv("DB_PASS")+"@/"+os.Getenv("DB_NAME")+dbOption)
+	if err != nil {
+		log.Fatal(err)
+	}
+	sqlHandler := new(SqlHandler)
+	sqlHandler.Conn = conn
+	return sqlHandler
+}
