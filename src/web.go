5738acbeb217cae4931fe57a398a704b0db5aaed
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-10-18 02:42:18 +0900

    main.goを書いた

--- src/web.go
+++ src/web.go
@@ -1,1 +1,17 @@
-package src
+package main
+
+import (
+	"fmt"
+	"github.com/labstack/echo"
+	"github.com/xxarupakaxx/go-todo-api/src/handler"
+	"github.com/xxarupakaxx/go-todo-api/src/injector"
+)
+
+func main() {
+	fmt.Println("server start ...")
+	todoHandler := injector.InjectTodoHandler()
+	e := echo.New()
+	handler.InitRouting(e,todoHandler)
+	e.Logger.Debug()
+	e.Logger.Fatal(e.Start(":8080"))
+}
