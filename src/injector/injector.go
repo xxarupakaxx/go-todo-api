4da1acf56b20c9f75e74f9d9461ec054f206ce9b
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-10-18 02:22:45 +0900

    🤕Injection

--- src/injector/injector.go
+++ src/injector/injector.go
@@ -1,1 +1,27 @@
 package injector
+
+import (
+	"github.com/xxarupakaxx/go-todo-api/src/domain/repository"
+	"github.com/xxarupakaxx/go-todo-api/src/handler"
+	"github.com/xxarupakaxx/go-todo-api/src/infra"
+	"github.com/xxarupakaxx/go-todo-api/src/usecase"
+)
+
+func InjectDB() infra.SqlHandler {
+	sqlhandler := infra.NewSqlHandler()
+	return *sqlhandler
+}
+
+func InjectTodoRepository() repository.TodoRepository {
+	sqlHandler := InjectDB()
+	return infra.NewTodoRepository(sqlHandler)
+}
+
+func InjectTodoUsecase() usecase.TodoUsecase {
+	todoRepo := InjectTodoRepository()
+	return usecase.NewTodoUsecase(todoRepo)
+}
+
+func InjectTodoHandler() handler.TodoHandler {
+	return handler.NewTodoHandler(InjectTodoUsecase())
+}
