128a5c1a0ebd6b904b5f4ebc22b3c233dd018deb
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-10-18 02:24:45 +0900

    :rooster: handlerの実装

--- src/handler/router.go
+++ src/handler/router.go
@@ -1,1 +1,10 @@
 package handler
+
+import "github.com/labstack/echo"
+
+func InitRouting(e *echo.Echo, todoHandler TodoHandler) {
+	e.GET("/",todoHandler.View())
+	e.GET("/search",todoHandler.Search())
+	e.POST("/create",todoHandler.Add())
+	e.POST("/edit",todoHandler.Edit())
+}
