57164fdc068b00ec31545d426c83300d08ee156d
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-10-18 16:30:30 +0900

    domainの実装

---
+++ LayeredArchitecture/domain/repository/user_repository.go
@@ -0,0 +1,11 @@
+package repository
+
+import (
+	"database/sql"
+	"github.com/xxarupakaxx/go-todo-api/LayeredArchitecture/domain"
+)
+
+type UserRepository interface {
+	Insert(DB *sql.DB,userID,name,email string) error
+	GetByUserID(DB *sql.DB,userID string)  (*domain.User,error)
+}
