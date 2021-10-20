c7b19304897e5241b3de73e3337f7cc02baada28
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-10-21 02:21:07 +0900

    interfaceの命名の変更

--- LayeredArchitecture/usecase/news.go
+++ LayeredArchitecture/usecase/news.go
@@ -9,70 +9,42 @@ type newsUseCase struct {
 	newsRepository repository.NewsRepository
 }
 
-func (n *newsUseCase) Get(id int) (*domain.News, error) {
-	news,err := n.newsRepository.Get(id)
-	if err != nil {
-		return nil, err
-	}
-	return news,nil
+func (n newsUseCase) GetNews(id int) (*domain.News, error) {
+	panic("implement me")
 }
 
-func (n *newsUseCase) GetAll() ([]domain.News, error) {
-	news,err := n.newsRepository.GetAll()
-	if err!=nil {
-		return nil, err
-	}
-	return news,nil
+func (n newsUseCase) GetAllNews() ([]domain.News, error) {
+	panic("implement me")
 }
 
-func (n *newsUseCase) GetBySlug(slug string) ([]*domain.News, error) {
-	news,err := n.newsRepository.GetBySlug(slug)
-	if err != nil {
-		return nil, err
-	}
-	return news,nil
+func (n newsUseCase) AddNews(news domain.News) error {
+	panic("implement me")
 }
 
-func (n *newsUseCase) GetAllByStatus(status string) ([]domain.News, error) {
-	news,err := n.newsRepository.GetAllByStatus(status)
-	if err != nil {
-		return nil, err
-	}
-	return news,err
+func (n newsUseCase) GetAllNewsByFilter(status string) ([]domain.News, error) {
+	panic("implement me")
 }
 
-func (n *newsUseCase) Save(news *domain.News) error {
-	err := n.newsRepository.Save(news)
-	if err != nil {
-		return err
-	}
-	return nil
+func (n newsUseCase) RemoveNews(id int) error {
+	panic("implement me")
 }
 
-func (n *newsUseCase) Remove(id int) error {
-	err := n.newsRepository.Remove(id)
-	if err != nil {
-		return err
-	}
-	return nil
+func (n newsUseCase) UpdateNews(news *domain.News) error {
+	panic("implement me")
 }
 
-func (n *newsUseCase) Update(news *domain.News) error {
-	err := n.newsRepository.Update(news)
-	if err != nil {
-		return err
-	}
-	return nil
+func (n newsUseCase) GetNewsByTopic(slug string) ([]*domain.News, error) {
+	panic("implement me")
 }
 
 type NewsUseCase interface {
-	Get(id int) (*domain.News,error)
-	GetAll() ([]domain.News,error)
-	GetBySlug(slug string)([]*domain.News,error)
-	GetAllByStatus(status string)([]domain.News,error)
-	Save(news *domain.News) error
-	Remove(id int) error
-	Update(news *domain.News) error
+	GetNews(id int) (*domain.News,error)
+	GetAllNews() ([]domain.News,error)
+	AddNews(news domain.News)error
+	GetAllNewsByFilter(status string) ([]domain.News, error)
+	RemoveNews(id int) error
+	UpdateNews(news *domain.News) error
+	GetNewsByTopic(slug string) ([]*domain.News, error)
 }
 
 func newNewsUseCase(newsRepository repository.NewsRepository) NewsUseCase {
