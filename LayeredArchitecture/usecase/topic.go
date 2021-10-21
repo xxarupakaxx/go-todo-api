e96fb2e4a0d090f7fa4abac855061b7ab0e976b1
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-10-21 15:08:13 +0900

    TopicのUseCaseの実装

--- LayeredArchitecture/usecase/topic.go
+++ LayeredArchitecture/usecase/topic.go
@@ -9,50 +9,59 @@ type topicUseCase struct {
 	topicRepository repository.TopicRepository
 }
 
-func (t *topicUseCase) Get(id int) (*domain.Topic, error) {
+func (t *topicUseCase) GetTopic(id int) (*domain.Topic, error) {
+
 	topic,err := t.topicRepository.Get(id)
 	if err != nil {
-		return nil, err
+		return nil,err
 	}
-	return topic,nil
+	return topic,err
 }
 
-func (t *topicUseCase) GetAll() ([]domain.Topic, error) {
-	topics,err := t.topicRepository.GetAll()
+func (t *topicUseCase) GetAllTopic() ([]*domain.Topic, error) {
+	topics,err:= t.topicRepository.GetAll()
 	if err != nil {
 		return nil, err
 	}
-	return topics,nil
+	return topics,err
 }
 
-func (t *topicUseCase) Save(topic *domain.Topic) error {
-	err := t.topicRepository.Save(topic)
+func (t *topicUseCase) AddTopic(name, slug string) error {
+	u:= domain.Topic{
+		Name: name,
+		Slug: slug,
+	}
+	return t.topicRepository.Save(&u)
+}
+
+func (t *topicUseCase) UpdateTopic(topic *domain.Topic, id int) error {
+	err:=t.topicRepository.Update(topic)
 	if err != nil {
+		return err
+	}
+	topic.ID = uint(id)
+	return nil
+}
+
+func (t *topicUseCase) Remove(id int) error {
+	if err := t.topicRepository.Remove(id); err != nil {
 		return err
 	}
 	return nil
 }
 
-func (t *topicUseCase) Remove(id int) error {
-	err := t.topicRepository.Remove(id)
-	if err != nil {
-		return err
-	}
-	return err
-}
-
 func (t *topicUseCase) Update(topic *domain.Topic) error {
-	err := t.topicRepository.Update(topic)
-	if err != nil {
+	if err := t.topicRepository.Update(topic); err != nil {
 		return err
 	}
 	return nil
 }
 
 type TopicUseCase interface {
-	Get(id int)(*domain.Topic,error)
-	GetAll()([]domain.Topic,error)
-	Save(topic *domain.Topic) error
+	GetTopic(id int)(*domain.Topic,error)
+	GetAllTopic()([]*domain.Topic,error)
+	AddTopic(name,slug string) error
+	UpdateTopic(topic *domain.Topic,id int) error
 	Remove(id int) error
 	Update(topic *domain.Topic) error
 }
