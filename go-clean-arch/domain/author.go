3fde0a32b847483abe58c8c5e735c9509fcff5fa
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-11-07 17:12:41 +0900

    Authorのdomain

--- go-clean-arch/domain/author.go
+++ go-clean-arch/domain/author.go
@@ -1,1 +1,14 @@
 package domain
+
+import "context"
+
+type Author struct {
+	ID int64 `json:"id"`
+	Name string `json:"name"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+}
+
+type AuthorRepository interface {
+	GetByID(ctx context.Context,id int64)(Author,error)
+}
