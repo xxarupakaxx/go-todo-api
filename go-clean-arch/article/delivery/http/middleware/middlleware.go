3168aa0cde4b452c53a22b3d88026ba7bfe30dc9
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-11-08 01:19:55 +0900

    middleware

--- go-clean-arch/article/delivery/http/middleware/middlleware.go
+++ go-clean-arch/article/delivery/http/middleware/middlleware.go
@@ -1,1 +1,18 @@
 package middleware
+
+import "github.com/labstack/echo/v4"
+
+type GoMiddleware struct {
+
+}
+
+func (m *GoMiddleware) CORS(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		c.Response().Header().Set("Access-Control-Allow-Origin","*")
+		return next(c)
+	}
+}
+
+func InitMiddleware() *GoMiddleware {
+	return &GoMiddleware{}
+}
