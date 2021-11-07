6391629f5aa494473e126a2ffbd3250876b8b14d
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-11-07 17:15:02 +0900

    errors.go

--- go-clean-arch/domain/errors.go
+++ go-clean-arch/domain/errors.go
@@ -1,1 +1,10 @@
 package domain
+
+import "errors"
+
+var (
+	ErrInternalServer = errors.New("internal Server Error")
+	ErrNotFound = errors.New("your requested Item is not found")
+	ErrConflict = errors.New("your Item already exist")
+	ErrBadParamInput = errors.New("given Param is not valid")
+)
