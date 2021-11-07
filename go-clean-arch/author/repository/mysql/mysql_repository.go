d1ad53ef2adfee7a59bb5da34b08ca2aad871bf3
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-11-07 23:23:34 +0900

    authorのrepo

--- go-clean-arch/author/repository/mysql/mysql_repository.go
+++ go-clean-arch/author/repository/mysql/mysql_repository.go
@@ -1,1 +1,34 @@
 package mysql
+
+import (
+	"context"
+	"database/sql"
+	"github.com/xxarupakaxx/go-todo-api/go-clean-arch/domain"
+)
+
+type mysqlAuthorRepo struct {
+	DB *sql.DB
+}
+
+func (m *mysqlAuthorRepo) GetByID(ctx context.Context, id int64) (domain.Author, error) {
+	query := `SELECT id,name, created_at, updated_at FROM author WHERE id=?`
+	return m.getOne(ctx,query,id)
+}
+
+func (m *mysqlAuthorRepo) getOne(ctx context.Context, query string, args ...interface{})(res domain.Author,err error)  {
+	stmt,err := m.DB.PrepareContext(ctx,query)
+	if err != nil {
+		return domain.Author{},err
+	}
+
+	row := stmt.QueryRowContext(ctx,args...)
+	res = domain.Author{}
+	err = row.Scan(&res.ID,&res.Name,&res.CreatedAt,&res.UpdatedAt)
+	return
+}
+
+func NewMysqlAuthorRepository(db *sql.DB) domain.AuthorRepository {
+	return &mysqlAuthorRepo{DB: db}
+}
+
+
